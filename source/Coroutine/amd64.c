b630729ad6647aedd664379f1d488e3e7a9ab891
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2018-05-10 16:22:42 +1200

    Don't use naked, it doesn't seem compatible with gcc.

--- source/Coroutine/amd64.c
+++ source/Coroutine/amd64.c
@@ -8,31 +8,38 @@
 
 #include "amd64.h"
 
-__attribute__((naked))
-coroutine_context * coroutine_transfer(coroutine_context * current, coroutine_context * target)
-{
-	asm(
-		/* Save caller state */
-		"\tpushq %rbp\n"
-		"\tpushq %rbx\n"
-		"\tpushq %r12\n"
-		"\tpushq %r13\n"
-		"\tpushq %r14\n"
-		"\tpushq %r15\n"
-		/* Save caller stack pointer */
-		"\tmovq %rsp, (%rdi)\n"
-		/* Restore callee stack pointer */
-		"\tmovq (%rsi), %rsp\n"
-		/* Restore callee stack */
-		"\tpopq %r15\n"
-		"\tpopq %r14\n"
-		"\tpopq %r13\n"
-		"\tpopq %r12\n"
-		"\tpopq %rbx\n"
-		"\tpopq %rbp\n"
-		/* Put the first argument into the return value */
-		"\tmovq %rdi, %rax\n"
-		/* We pop the return address and jump to it */
-		"\tret\n"
-	);
-}
+// coroutine_context * coroutine_transfer(coroutine_context * current, coroutine_context * target)
+asm(
+	"\t.text\n"
+	
+	#if _WIN32 || __CYGWIN__ || __MACH__
+	"\t.globl _coroutine_transfer\n"
+	"_coroutine_transfer:\n"
+	#else
+	"\t.globl coroutine_transfer\n"
+	"coroutine_transfer:\n"
+	#endif
+	
+	/* Save caller state */
+	"\tpushq %rbp\n"
+	"\tpushq %rbx\n"
+	"\tpushq %r12\n"
+	"\tpushq %r13\n"
+	"\tpushq %r14\n"
+	"\tpushq %r15\n"
+	/* Save caller stack pointer */
+	"\tmovq %rsp, (%rdi)\n"
+	/* Restore callee stack pointer */
+	"\tmovq (%rsi), %rsp\n"
+	/* Restore callee stack */
+	"\tpopq %r15\n"
+	"\tpopq %r14\n"
+	"\tpopq %r13\n"
+	"\tpopq %r12\n"
+	"\tpopq %rbx\n"
+	"\tpopq %rbp\n"
+	/* Put the first argument into the return value */
+	"\tmovq %rdi, %rax\n"
+	/* We pop the return address and jump to it */
+	"\tret\n"
+);
